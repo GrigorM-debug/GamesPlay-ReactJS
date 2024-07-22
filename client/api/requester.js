5b1f0d40a75a361af1351abf0111b07cb595c6c1
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-07-23 02:28:49 +0300

    requester refactor

--- client/api/requester.js
+++ client/api/requester.js
@@ -21,6 +21,6 @@ export async function requester(method, url, data) {
 }
 
 export const get = requester.bind(null, 'GET');
-export const post = () => requester.bind(null, 'POST');
-export const put = () => requester.bind(null, 'PUT');
-export const del = () => requester.bind(null, 'DELETE');
+export const post = requester.bind(null, 'POST');
+export const put = requester.bind(null, 'PUT');
+export const del = requester.bind(null, 'DELETE');
