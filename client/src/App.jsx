313b35d1bef10231304130c1758cb3613082aae8
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-07-23 00:59:49 +0300

    Added some routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 
+
+import Home from "./components/home/Home";
+import Register from "./components/register/Register";
+import Login from "./components/login/login";
+import Create from "./components/create/Create";
 
 function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/create" element={<Create />}></Route>
 
-  return (
-    
-  );
+        </Routes>
+    );
 }
 
 export default App
